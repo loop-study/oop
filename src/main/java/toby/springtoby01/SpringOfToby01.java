9dd0aceef14632b08cc8c13e43d8af24b0ef069f
apdkeb <apdkeb@naver.com>
2021-06-26 12:00:06 +0900

    토비의스프링 main() 테스트 추가

---
+++ src/main/java/toby/springtoby01/SpringOfToby01.java
@@ -0,0 +1,24 @@
+package toby.springtoby01;
+
+import java.sql.SQLException;
+
+public class SpringOfToby01 {
+
+    public static void main(String[] args) throws SQLException, ClassNotFoundException {
+        UserDao userDao = new UserDao();
+
+        User user = new User();
+        user.setId("loop");
+        user.setName("반복");
+        user.setPassword("study");
+
+        userDao.add(user);
+
+        System.out.println(user.getId() + " 등록 성공");
+
+        User user2 = userDao.get(user.getId());
+        System.out.println(user2.getName());
+        System.out.println(user2.getPassword());
+        System.out.println(user2.getId() + " 조회 성공");
+    }
+}
