d6e9556931ef92e7b4984e4a5aba5a201aea8454
apdkeb <apdkeb@naver.com>
2021-06-26 15:22:51 +0900

    인터페이스로 DB 커넥션 생성

---
+++ src/main/java/toby/springtoby01/ConnectionMaker.java
@@ -0,0 +1,8 @@
+package toby.springtoby01;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+public interface ConnectionMaker {
+    public Connection makeConnection() throws ClassNotFoundException, SQLException;
+}
--- src/main/java/toby/springtoby01/UserDao.java
+++ src/main/java/toby/springtoby01/UserDao.java
@@ -4,14 +4,14 @@ package toby.springtoby01;
 import java.sql.*;
 
 public class UserDao {
-    private SimpleConnectionMaker simpleConnectionMaker;
+    private ConnectionMaker connectionMaker;
 
     public UserDao() {
-        this.simpleConnectionMaker = new SimpleConnectionMaker();
+        this.connectionMaker = new DConnectionMaker();
     }
 
     public void add(User user) throws ClassNotFoundException, SQLException {
-        Connection c = simpleConnectionMaker.getConnection();
+        Connection c = connectionMaker.makeConnection();
 
         PreparedStatement ps = c.prepareStatement("insert into users(id, name, password) value(?,?,?)");
         ps.setString(1, user.getId());
@@ -25,7 +25,7 @@ public class UserDao {
     }
 
     public User get(String id) throws ClassNotFoundException, SQLException {
-        Connection c = simpleConnectionMaker.getConnection();
+        Connection c = connectionMaker.makeConnection();
 
         PreparedStatement ps = c.prepareStatement("select * from users where id = ?");
         ps.setString(1, id);
