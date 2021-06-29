66231e80fc8dcc108195aabad6a55e1e4d9534f2
apdkeb <apdkeb@naver.com>
2021-06-29 17:05:07 +0900

    @Before 추가

--- src/test/java/toby/springOfToby02/UserDaoTest.java
+++ src/test/java/toby/springOfToby02/UserDaoTest.java
@@ -1,6 +1,8 @@
 package toby.springOfToby02;
 
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.GenericXmlApplicationContext;
@@ -14,25 +16,31 @@ import static org.assertj.core.api.Assertions.*;
 
 
 public class UserDaoTest {
+    // DAO는 같은걸 사용하니 인스턴스 변수로 선언한다.
+    private UserDao dao;
+
+    // @Test 메소드가 실행되기전에 @BeforeEach 실행된다.
+    @BeforeAll
+    public void setUp() {
+        ApplicationContext context = new GenericXmlApplicationContext("toby02/applicationContext.xml");
+        dao = context.getBean("userDao", UserDao.class);
+    }
 
     @Test
     public void addAndGet() throws Exception {
-        ApplicationContext context = new GenericXmlApplicationContext("toby02/applicationContext.xml");
-        UserDao userDao = context.getBean("userDao", UserDao.class);
-
         // 테이블 데이터 초기화
-        userDao.allDelete();
-        assertThat(userDao.getCount()).isEqualTo(0);
+        dao.allDelete();
+        assertThat(dao.getCount()).isEqualTo(0);
 
         User user = new User();
         user.setId("loop");
         user.setName("반복");
         user.setPassword("study");
 
-        userDao.add(user);
-        assertThat(userDao.getCount()).isEqualTo(1);
+        dao.add(user);
+        assertThat(dao.getCount()).isEqualTo(1);
 
-        User user2 = userDao.get(user.getId());
+        User user2 = dao.get(user.getId());
 
         assertThat(user.getName()).isEqualTo(user2.getName());
         assertThat(user.getPassword()).isEqualTo(user2.getPassword());
@@ -40,14 +48,11 @@ public class UserDaoTest {
 
     @Test
     public void getUserFailure() throws SQLException, ClassNotFoundException {
-        ApplicationContext context = new GenericXmlApplicationContext("toby02/applicationContext.xml");
-        UserDao userDao = context.getBean("userDao", UserDao.class);
-
-        userDao.allDelete();
-        assertThat(userDao.getCount()).isEqualTo(0);
+        dao.allDelete();
+        assertThat(dao.getCount()).isEqualTo(0);
 
         assertThatThrownBy(() -> {
-            userDao.get("unknown_id");
+            dao.get("unknown_id");
         }).isInstanceOf(EmptyResultDataAccessException.class);
     }
 }
