e02871c092e392280b52fd4c029aea75d2cb44ea
apdkeb <apdkeb@naver.com>
2021-07-17 15:50:02 +0900

    자바플레이그라운드 - 입력 받은 두 숫자 합, 풀이

--- src/main/java/javaplayground/Calendar.java
+++ src/main/java/javaplayground/Calendar.java
@@ -5,12 +5,14 @@ import java.util.Scanner;
 public class Calendar {
 
     public static void main(String[] args) {
-        System.out.println("두 수를 입력하세요. 스페이스로 띄우기 ex)7 10");
-        Scanner scanner = new Scanner(System.in);
-        String inputValue = scanner.nextLine();
-        String[] splitedValue = inputValue.split(" ");
-        int first = Integer.parseInt(splitedValue[0]);
-        int second = Integer.parseInt(splitedValue[1]);
-        System.out.println("두 수의 합은 " + (first + second) + "입니다");
+//        System.out.println("두 수를 입력하세요. 스페이스로 띄우기 ex)7 10");
+//        Scanner scanner = new Scanner(System.in);
+//        String inputValue = scanner.nextLine();
+//        String[] splitedValue = inputValue.split(" ");
+//        int first = Integer.parseInt(splitedValue[0]);
+//        int second = Integer.parseInt(splitedValue[1]);
+//        System.out.println("두 수의 합은 " + (first + second) + "입니다");
+
+
     }
 }
